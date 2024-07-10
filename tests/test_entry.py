57f2f951672557ce9e24009e02b664e342257740
Ilia Matveev <ilyamatveev599@gmail.com>
2024-07-10 21:47:01 +0700

    Исправлено название файла с локаторами, добавлен файл с переменными для юрл и тестовых данных.

--- tests/test_entry.py
+++ tests/test_entry.py
@@ -1,32 +1,33 @@
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-from Locators_description import TestLocators
+from locators import TestLocators
+from test_data import STARTING_PAGE, EMAIL_TEST, PASSWORD_TEST
 
 
 class TestEntry:
     def test_entry_by_login_button_success(self, driver):
-        driver.get("https://stellarburgers.nomoreparties.site/")
+        driver.get(STARTING_PAGE)
 
         driver.find_element(*TestLocators.SEARCH_LOGIN_BUTTON_MAIN_PAGE).click()
-        driver.find_element(*TestLocators.SEARCH_INPUT_EMAIL_ACCOUNT_ENTER).send_keys("ilyamatveev1@gmail.com")
-        driver.find_element(*TestLocators.SEARCH_INPUT_PASSWORD_ACCOUNT_ENTER).send_keys("elirose")
+        driver.find_element(*TestLocators.SEARCH_INPUT_EMAIL_ACCOUNT_ENTER).send_keys(EMAIL_TEST)
+        driver.find_element(*TestLocators.SEARCH_INPUT_PASSWORD_ACCOUNT_ENTER).send_keys(PASSWORD_TEST)
         driver.find_element(*TestLocators.SEARCH_LOGIN_BUTTON).click()
 
-        WebDriverWait(driver, 5).until(expected_conditions.url_to_be("https://stellarburgers.nomoreparties.site/"))
+        WebDriverWait(driver, 5).until(expected_conditions.url_to_be(STARTING_PAGE))
 
-        assert driver.current_url == "https://stellarburgers.nomoreparties.site/"
+        assert driver.current_url == STARTING_PAGE
 
     def test_entry_by_personal_account_button_success(self, driver):
-        driver.get("https://stellarburgers.nomoreparties.site/")
+        driver.get(STARTING_PAGE)
 
         driver.find_element(*TestLocators.SEARCH_PERSONAL_ACCOUNT_BUTTON).click()
-        driver.find_element(*TestLocators.SEARCH_INPUT_EMAIL_ACCOUNT_ENTER).send_keys("ilyamatveev1@gmail.com")
-        driver.find_element(*TestLocators.SEARCH_INPUT_PASSWORD_ACCOUNT_ENTER).send_keys("elirose")
+        driver.find_element(*TestLocators.SEARCH_INPUT_EMAIL_ACCOUNT_ENTER).send_keys(EMAIL_TEST)
+        driver.find_element(*TestLocators.SEARCH_INPUT_PASSWORD_ACCOUNT_ENTER).send_keys(PASSWORD_TEST)
         driver.find_element(*TestLocators.SEARCH_LOGIN_BUTTON).click()
 
-        WebDriverWait(driver, 5).until(expected_conditions.url_to_be("https://stellarburgers.nomoreparties.site/"))
+        WebDriverWait(driver, 5).until(expected_conditions.url_to_be(STARTING_PAGE))
 
-        assert driver.current_url == "https://stellarburgers.nomoreparties.site/"
+        assert driver.current_url == STARTING_PAGE
 
     def test_entry_by_registration_screen_login_button_success(self, driver):
         driver.get("https://stellarburgers.nomoreparties.site/")
@@ -38,20 +39,20 @@ class TestEntry:
         driver.find_element(*TestLocators.SEARCH_INPUT_PASSWORD_ACCOUNT_ENTER).send_keys("elirose")
         driver.find_element(*TestLocators.SEARCH_LOGIN_BUTTON).click()
 
-        WebDriverWait(driver, 5).until(expected_conditions.url_to_be("https://stellarburgers.nomoreparties.site/"))
+        WebDriverWait(driver, 5).until(expected_conditions.url_to_be(STARTING_PAGE))
 
-        assert driver.current_url == "https://stellarburgers.nomoreparties.site/"
+        assert driver.current_url == STARTING_PAGE
 
     def test_entry_by_password_restoration_login_button_success(self, driver):
-        driver.get("https://stellarburgers.nomoreparties.site/")
+        driver.get(STARTING_PAGE)
 
         driver.find_element(*TestLocators.SEARCH_LOGIN_BUTTON_MAIN_PAGE).click()
         driver.find_element(*TestLocators.SEARCH_PASSWORD_RESTORATION_BUTTON).click()
         driver.find_element(*TestLocators.SEARCH_PASSWORD_RESTORATION_LOGIN_BUTTON).click()
-        driver.find_element(*TestLocators.SEARCH_INPUT_EMAIL_ACCOUNT_ENTER).send_keys("ilyamatveev1@gmail.com")
-        driver.find_element(*TestLocators.SEARCH_INPUT_PASSWORD_ACCOUNT_ENTER).send_keys("elirose")
+        driver.find_element(*TestLocators.SEARCH_INPUT_EMAIL_ACCOUNT_ENTER).send_keys(EMAIL_TEST)
+        driver.find_element(*TestLocators.SEARCH_INPUT_PASSWORD_ACCOUNT_ENTER).send_keys(PASSWORD_TEST)
         driver.find_element(*TestLocators.SEARCH_LOGIN_BUTTON).click()
 
-        WebDriverWait(driver, 5).until(expected_conditions.url_to_be("https://stellarburgers.nomoreparties.site/"))
+        WebDriverWait(driver, 5).until(expected_conditions.url_to_be(STARTING_PAGE))
 
-        assert driver.current_url == "https://stellarburgers.nomoreparties.site/"
+        assert driver.current_url == STARTING_PAGE
--- tests/test_personal_account.py
+++ tests/test_personal_account.py
@@ -1,29 +1,30 @@
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-from Locators_description import TestLocators
+from locators import TestLocators
+from test_data import STARTING_PAGE, LOGIN_PAGE, PROFILE_PAGE, EMAIL_TEST, PASSWORD_TEST
 
 
 class TestPersonalAccount:
     def test_go_to_account_by_personal_account_button_success(self, driver):
-        driver.get("https://stellarburgers.nomoreparties.site/")
+        driver.get(STARTING_PAGE)
 
         driver.find_element(*TestLocators.SEARCH_LOGIN_BUTTON_MAIN_PAGE).click()
-        driver.find_element(*TestLocators.SEARCH_INPUT_EMAIL_ACCOUNT_ENTER).send_keys("ilyamatveev1@gmail.com")
-        driver.find_element(*TestLocators.SEARCH_INPUT_PASSWORD_ACCOUNT_ENTER).send_keys("elirose")
+        driver.find_element(*TestLocators.SEARCH_INPUT_EMAIL_ACCOUNT_ENTER).send_keys(EMAIL_TEST)
+        driver.find_element(*TestLocators.SEARCH_INPUT_PASSWORD_ACCOUNT_ENTER).send_keys(PASSWORD_TEST)
         driver.find_element(*TestLocators.SEARCH_LOGIN_BUTTON).click()
         driver.find_element(*TestLocators.SEARCH_PERSONAL_ACCOUNT_BUTTON).click()
 
         WebDriverWait(driver, 5).until(
-            expected_conditions.url_to_be("https://stellarburgers.nomoreparties.site/account/profile"))
+            expected_conditions.url_to_be(PROFILE_PAGE))
 
-        assert driver.current_url == "https://stellarburgers.nomoreparties.site/account/profile"
+        assert driver.current_url == PROFILE_PAGE
 
     def test_logout_by_logout_button_success(self, driver):
-        driver.get("https://stellarburgers.nomoreparties.site/")
+        driver.get(STARTING_PAGE)
 
         driver.find_element(*TestLocators.SEARCH_LOGIN_BUTTON_MAIN_PAGE).click()
-        driver.find_element(*TestLocators.SEARCH_INPUT_EMAIL_ACCOUNT_ENTER).send_keys("ilyamatveev1@gmail.com")
-        driver.find_element(*TestLocators.SEARCH_INPUT_PASSWORD_ACCOUNT_ENTER).send_keys("elirose")
+        driver.find_element(*TestLocators.SEARCH_INPUT_EMAIL_ACCOUNT_ENTER).send_keys(EMAIL_TEST)
+        driver.find_element(*TestLocators.SEARCH_INPUT_PASSWORD_ACCOUNT_ENTER).send_keys(PASSWORD_TEST)
         driver.find_element(*TestLocators.SEARCH_LOGIN_BUTTON).click()
         driver.find_element(*TestLocators.SEARCH_PERSONAL_ACCOUNT_BUTTON).click()
 
@@ -31,6 +32,6 @@ class TestPersonalAccount:
             expected_conditions.visibility_of_element_located(TestLocators.SEARCH_LOGOUT_BUTTON))
         driver.find_element(*TestLocators.SEARCH_LOGOUT_BUTTON).click()
 
-        WebDriverWait(driver, 5).until(expected_conditions.url_to_be("https://stellarburgers.nomoreparties.site/login"))
+        WebDriverWait(driver, 5).until(expected_conditions.url_to_be(LOGIN_PAGE))
 
-        assert driver.current_url == "https://stellarburgers.nomoreparties.site/login"
+        assert driver.current_url == LOGIN_PAGE
